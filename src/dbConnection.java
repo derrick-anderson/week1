14c6fcfa97525f12e128dd389b1d5a1c9efc8989
Derrick Anderson <deanderson@solstice.com>
2018-07-29 21:49:18 -0500

    Adds method to query and return summary bean(yet unused)

--- src/dbConnection.java
+++ src/dbConnection.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class dbConnection
@@ -17,7 +14,14 @@ public class dbConnection
     private static final String CONN_STRING = "jdbc:mysql://localhost/week1";
     private static String truncateSql = "TRUNCATE TABLE stock_quotes";
     private static String insertSql = "INSERT INTO stock_quotes (date, symbol, price, volume) VALUES (?,?,?,?)";
-
+    private static String summarySql = "SELECT symbol, DATE_FORMAT(date, ?) as date,  max(price)" +
+            " as max_price, min(price) as min_price, sum(volume) as trade_volume," +
+            " (SELECT price FROM stock_quotes WHERE symbol = ? AND DATE_FORMAT(date, ?) = ? " +
+            " order by date DESC LIMIT 1) as closing_price ," +
+            " (SELECT price FROM stock_quotes WHERE symbol = ? AND DATE_FORMAT(date, ?) = ? " +
+            " order by date ASC LIMIT 1) as opening_price " +
+            " from stock_quotes WHERE symbol = ? AND DATE_FORMAT(date, ?) = ?" +
+            " group by symbol, DATE_FORMAT(date, ?);";
 
     // Singleton Connection Constructor
     private static Connection dbConnection()
@@ -57,7 +61,7 @@ public class dbConnection
 
     }
 
-    //todo: Add a method for the batch insert to the table.
+
     // Method to do bulk insert to table.
     public static void insertToTable(List<StockQuote> list_quotes){
         conn = getConnection();
@@ -81,7 +85,50 @@ public class dbConnection
         }
     }
 
-    //todo: Add a method for the query for the stocks
+    //todo: Add a method for the query for the summary
+    // Method that queries table for summary and returns summary bean.
+    public static StockSummary getSummary(String ticker, String date, String dateFormat){
+
+        conn = getConnection();
+        StockSummary bean = new StockSummary();
+
+        try{
+            PreparedStatement summaryCall = conn.prepareStatement(summarySql);
+
+            summaryCall.setString(1, dateFormat);
+            summaryCall.setString(2, ticker);
+            summaryCall.setString(3, dateFormat);
+            summaryCall.setString(4, date);
+            summaryCall.setString(5, ticker);
+            summaryCall.setString(6, dateFormat);
+            summaryCall.setString(7, date);
+            summaryCall.setString(8, ticker);
+            summaryCall.setString(9, dateFormat);
+            summaryCall.setString(10, date);
+            summaryCall.setString(11, dateFormat);
+
+            ResultSet rs = summaryCall.executeQuery();
+
+            rs.next();
+
+            bean.setTicker(rs.getString("symbol"));
+            bean.setVolume(rs.getInt("trade_volume"));
+            bean.setOpen_price(rs.getBigDecimal("opening_price"));
+            bean.setLow_price(rs.getBigDecimal("min_price"));
+            bean.setHigh_price(rs.getBigDecimal("max_price"));
+            bean.setClosing_price(rs.getBigDecimal("closing_price"));
+
+            rs.close();
+
+        }
+        catch(SQLException e){
+            printSqlError(e);
+            System.err.println("Issue Writing Quotes to DB!");
+        }
+        finally{
+            return bean;
+        }
+    }
 
     //todo: Add a method to return a list of the Symbols in the table
 
