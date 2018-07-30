c5c06ab9221aeed7eaae8066ecc1c8c2fcf2bd01
Derrick Anderson <deanderson@solstice.com>
2018-07-30 00:10:45 -0500

    Adds method to input handler to create stock bean list

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,5 +1,10 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
 
 public class InputHelper {
 
@@ -34,4 +39,21 @@ public class InputHelper {
         String input = getInput(prompt);
         return Integer.parseInt(input);
     }
+
+    //todo: Adding a method to pull data from url and return a list of StockQuotes
+    public static List<StockQuote> getStockQuotes(){
+        List<StockQuote> stock_list = null;
+        try{
+            URL input_url = new URL("https://bootcamp-training-files.cfapps.io/week1/week1-stocks.json");
+            ObjectMapper mapper = new ObjectMapper();
+            stock_list = mapper.readValue(input_url, new TypeReference<List<StockQuote>>() {});
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        finally {
+            return stock_list;
+        }
+    }
+
 }
